68e2a1ca16c79a76c6cccf4a936f93bbfcaead61
Mushthak <mail.musthak@gmail.com>
2022-10-03 14:15:40 +0530

    whish list added

--- routes/fetchRoutes.js
+++ routes/fetchRoutes.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const fetchController = require("../controller/fetchProductController")
 const vendorController = require("../controller/vendorController")
+const whishlistController = require("../controller/whishlistController")
 
 router.get("/all-turf" , fetchController.getAllProducts)
 
 router.get("/vendor-turf/:id", vendorController.vendorTurf)
+
+router.post("/add-whishlist" , whishlistController.addWhishList)
+
+router.get("/get-whishlist/:id" , whishlistController.getWhishList)
 
 module.exports = router 
