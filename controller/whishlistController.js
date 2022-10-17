18935ea311868ba19f1d48ec3973bd48670a3a50
Mushthak <100484745+MushthakVP@users.noreply.github.com>
2022-10-17 17:23:59 +0530

    favorite bug fixing

--- controller/whishlistController.js
+++ controller/whishlistController.js
@@ -1,85 +1,87 @@
-const asyncHandler = require("express-async-handler")
-const Whishlist = require("../schema/whishlistModel")
-
+const asyncHandler = require("express-async-handler");
+const Whishlist = require("../schema/whishlistModel");
 
 module.exports = {
-    addWhishList: asyncHandler(async (req, res) => {
-        try {
-            const dbObj = req.body
+  addWhishList: asyncHandler(async (req, res) => {
+    try {
+      const dbObj = req.body;
 
-            const findProduct = await Whishlist.findOne({ turf_name: dbObj.data[0].turf_name })
+      const findProduct = await Whishlist.findOne({
+        turf_name: dbObj.data[0].turf_name,
+      });
 
-            if (findProduct) {
-                res.status(401).json({ message: "Product already added to wishlist" })
-            }
+      if (findProduct) {
+        res.status(401).json({ message: "Product already added to wishlist" });
+      } else {
+        const whishlist = Whishlist({
+          turf_user_id: dbObj.turf_user_id,
+          turf_creator_id: dbObj.data[0].turf_creator_id,
+          turf_name: dbObj.data[0].turf_name,
+          turf_place: dbObj.data[0].turf_place,
+          turf_muncipality: dbObj.data[0].turf_muncipality,
+          turf_district: dbObj.data[0].turf_district,
+          turf_catogery: {
+            turf_cricket: dbObj.data[0].turf_catogery.turf_cricket,
+            turf_football: dbObj.data[0].turf_catogery.turf_football,
+            turf_badminton: dbObj.data[0].turf_catogery.turf_badminton,
+            turf_yoga: dbObj.data[0].turf_catogery.turf_yoga,
+          },
+          turf_type: {
+            turf_sevens: dbObj.data[0].turf_type.turf_sevens,
+            turf_sixes: dbObj.data[0].turf_type.turf_sixes,
+          },
+          turf_info: {
+            turf_isAvailale: dbObj.data[0].turf_info.turf_isAvailale,
+            turf_rating: dbObj.data[0].turf_info.turf_rating,
+            turf_map: dbObj.data[0].turf_info.turf_map,
+          },
+          turf_amenities: {
+            turf_washroom: dbObj.data[0].turf_amenities.turf_washroom,
+            turf_water: dbObj.data[0].turf_amenities.turf_water,
+            turf_dressing: dbObj.data[0].turf_amenities.turf_dressing,
+            turf_parking: dbObj.data[0].turf_amenities.turf_parking,
+            turf_gallery: dbObj.data[0].turf_amenities.turf_gallery,
+            turf_cafeteria: dbObj.data[0].turf_amenities.turf_cafeteria,
+          },
+          turf_images: {
+            turf_images1: dbObj.data[0].turf_images.turf_images1,
+            turf_images2: dbObj.data[0].turf_images.turf_images2,
+            turf_images3: dbObj.data[0].turf_images.turf_images3,
+          },
+          turf_time: {
+            time_morning: dbObj.data[0].turf_time.time_morning,
+            time_afternoon: dbObj.data[0].turf_time.time_afternoon,
+            time_evening: dbObj.data[0].turf_time.time_evening,
+          },
+        });
 
-            const whishlist = Whishlist({
-                turf_user_id: dbObj.turf_user_id,
-                turf_creator_id: dbObj.data[0].turf_creator_id,
-                turf_name: dbObj.data[0].turf_name,
-                turf_place: dbObj.data[0].turf_place,
-                turf_muncipality: dbObj.data[0].turf_muncipality,
-                turf_district: dbObj.data[0].turf_district,
-                turf_catogery:
-                {
-                    turf_cricket: dbObj.data[0].turf_catogery.turf_cricket,
-                    turf_football: dbObj.data[0].turf_catogery.turf_football,
-                    turf_badminton: dbObj.data[0].turf_catogery.turf_badminton,
-                    turf_yoga: dbObj.data[0].turf_catogery.turf_yoga,
-                },
-                turf_type: {
-                    turf_sevens: dbObj.data[0].turf_type.turf_sevens,
-                    turf_sixes: dbObj.data[0].turf_type.turf_sixes,
-                }, turf_info: {
-                    turf_isAvailale: dbObj.data[0].turf_info.turf_isAvailale,
-                    turf_rating: dbObj.data[0].turf_info.turf_rating,
-                    turf_map: dbObj.data[0].turf_info.turf_map,
-                },
-                turf_amenities: {
-                    turf_washroom: dbObj.data[0].turf_amenities.turf_washroom,
-                    turf_water: dbObj.data[0].turf_amenities.turf_water,
-                    turf_dressing: dbObj.data[0].turf_amenities.turf_dressing,
-                    turf_parking: dbObj.data[0].turf_amenities.turf_parking,
-                    turf_gallery: dbObj.data[0].turf_amenities.turf_gallery,
-                    turf_cafeteria: dbObj.data[0].turf_amenities.turf_cafeteria
-                },
-                turf_images: {
-                    turf_images1: dbObj.data[0].turf_images.turf_images1,
-                    turf_images2: dbObj.data[0].turf_images.turf_images2,
-                    turf_images3: dbObj.data[0].turf_images.turf_images3
-                },
-                turf_time: {
-                    time_morning: dbObj.data[0].turf_time.time_morning,
-                    time_afternoon: dbObj.data[0].turf_time.time_afternoon,
-                    time_evening: dbObj.data[0].turf_time.time_evening
-                }
+        await whishlist.save();
 
+        res.status(200).json({ status: true });
+      }
+    } catch (error) {
+      res.status(401).json({ status: false, message: error });
+    }
+  }),
 
-            })
+  getWhishList: asyncHandler(async (req, res, next) => {
+    const turf_user_id = req.params.id;
+    try {
+      const findWhishList = await Whishlist.find({
+        turf_user_id: turf_user_id,
+      });
 
-            await whishlist.save()
-
-            res.status(200).json({ "status": true })
-
-        } catch (error) {
-
-            res.status(401).json({ "status": false, "message": error })
-
-        }
-    }),
-
-
-    getWhishList: asyncHandler(async (req, res, next) => {
-        const turf_user_id = req.params.id
-        try {
-            const findWhishList = await Whishlist.find({ turf_user_id: turf_user_id })
-
-            res.status(200).json({ "status": true, "length": findWhishList.length, "data": findWhishList })
-
-        } catch (error) {
-            res.status(401).json({ "status": false, "length": 0, "data": `error ${error}` })
-        }
-
-
-    })
-}
+      res
+        .status(200)
+        .json({
+          status: true,
+          length: findWhishList.length,
+          data: findWhishList,
+        });
+    } catch (error) {
+      res
+        .status(401)
+        .json({ status: false, length: 0, data: `error ${error}` });
+    }
+  }),
+};
